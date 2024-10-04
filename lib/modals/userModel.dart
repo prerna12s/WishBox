42f4729ab5f182c6423d4baacb1f44581b022787
prerna12s <tripathiprerna1203@gmail.com>
2024-10-04 17:16:44 +0530

    first commit

---
+++ lib/modals/userModel.dart
@@ -0,0 +1,20 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'userModel.freezed.dart';
+// part 'userModel.g.dart';  // Uncomment this if you want to generate toJson/fromJson methods
+
+//freezed package that helps you to create modals without extra mess
+@freezed
+class UserModel with _$UserModel {
+  const factory UserModel({
+    required String uid,
+    String? email,
+    String? password,
+    String? profileImage,
+    @Default(false) bool isAdmin,
+  }) = _UserModel;
+
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => _$UserModelFromJson(json);
+}
+//got successss finally.....yeahhhh
